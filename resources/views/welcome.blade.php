2c050d17e37aa87bf4a6510b5120822d59ad48e6
Ahmet Özışık <ozisikahmet@gmail.com>
2015-11-18 11:47:38 +0200

    Coming soon

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,8 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">Coming Soon/div>
+                    <p>Gitesoft</p>
             </div>
         </div>
     </body>
