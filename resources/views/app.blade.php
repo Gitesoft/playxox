880eb02b2e8c310117462acb82bbfd44c633b53b
Ahmet Ozisik <ozisikahmet@gmail.com>
2015-11-19 01:39:26 +0200

    added loading screen

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,12 +4,16 @@
         <title>XOX</title>
         <link rel="stylesheet" type="text/css" href="{{ elixir('vendor.css') }}">
     </head>
-    <body>
-        <div class="container">
-            @yield('content')
+    <body ng-app="xox">
+
+        <div class="container text-center text-muted" ng-if="false">
+            <h1><i class="fa fa-circle-o-notch fa-spin"></i> Loading...</h1>
+        </div>
+        <div class="container" ng-cloak>
+            
         </div>
 
         <script type="text/javascript" src="{{ elixir('vendor.js') }}"></script>
-        <script type="text/javascript" src="{{ elixir('app.js') }}"></script>
+        <script type="text/javascript" src="{{ elixir('js/app.js') }}"></script>
     </body>
 </html>
