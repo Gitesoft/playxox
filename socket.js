81317e21fc3941716f65daee1f8ac69824886a69
Ahmet Ozisik <ozisikahmet@gmail.com>
2015-11-19 01:19:20 +0200

    moved socket server to main directory

--- socket.js
+++ socket.js
@@ -3,10 +3,12 @@
  */
 var server = require('http').createServer();
 var io = require('socket.io')(server);
+
 io.on('connection', function(socket){
   console.log('new connection');
 
   socket.on('event', function(data){});
   socket.on('disconnect', function(){});
 });
+
 server.listen(3000);
