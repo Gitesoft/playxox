1487ec5b0d78556338c69bc699c58ba3b4ee4ebc
Ahmet Özışık <ozisikahmet@gmail.com>
2015-11-19 11:36:15 +0200

    modified registration tests

--- tests/RegistrationTest.php
+++ tests/RegistrationTest.php
@@ -13,14 +13,39 @@ class RegistrationTest extends TestCase
      *
      * @return void
      */
-    public function testUserRegistration()
-    {
+
+    public function testEmptyNickname() {
         // Empty nickname
         $this->post('/api/register', ['nickname' => ''])->assertResponseStatus(422);
+    }
 
+    public function testActuallyEmptyNickname() {
+        // Empty nickname
+        // İki boş karakter diye bir kullanıcı adı olamaz
+        $this->post('/api/register', ['nickname' => '  '])->assertResponseStatus(422);
+    }
+
+    public function testInvalidCharacters() {
+        // ?, *, /, - gibi karakterlerden oluşamaz (regex kullanılacak, valid username?)
+        $this->post('/api/register', ['nickname' => '?'])->assertResponseStatus(422);
+        $this->post('/api/register', ['nickname' => '*'])->assertResponseStatus(422);
+    }
+
+    public function testTooShort() {
+        // En az üç karakterden oluşmalı
+        $this->post('/api/register', ['nickname' => 'a'])->assertResponseStatus(422);
+        $this->post('/api/register', ['nickname' => 'ab'])->assertResponseStatus(422);
+    }
+
+    public function testSuccessfulRegistration() {
         // Valid nickname
         $this->post('/api/register', ['nickname' => 'aozisik'])->assertResponseOk();
+    }
 
+    public function testUserRegistration()
+    {
+        // Valid nickname
+        $this->post('/api/register', ['nickname' => 'aozisik'])->assertResponseOk();
         // Invalid (non-unique) nickname
         $this->post('/api/register', ['nickname' => 'aozisik'])->assertResponseStatus(422);
     }
