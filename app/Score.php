28809f840273103e569d6fcd064073916251a082
onurkocak <onur.kocak@yahoo.com>
2015-11-19 14:14:32 +0000

    Scores table added, Fixed #9

---
+++ app/Score.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Score extends Model
+{
+    //
+    private $id;
+    private $player_1;
+    private $player_2;
+    private $winnerId;
+    private $timestamp;
+
+
+}
