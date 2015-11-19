e66b8bb3c516f30b79d364b763a3c173869f3b9c
ilterocal <ilterocal@gmail.com>
2015-11-19 15:05:12 +0200

    register from frontend to api completed. fixed #2

--- resources/assets/js/controllers/LoginCtrl.js
+++ resources/assets/js/controllers/LoginCtrl.js
@@ -5,25 +5,27 @@ xox.controller('LoginCtrl', ['$scope', 'locker', '$location', 'api', function ($
     }
 
     $scope.nickname = "";
+    $scope.errors = null;
 
     $scope.doLogin = function () {
 
         var success = function (response) {
+            $scope.errors = null;
             api.me = response;
             locker.put('me', response);
             $location.path('/lobby');
         };
 
-        success({
-            "id": Math.random() % 100,
-            "nickname": "aozisik",
-            "country": "United States",
-            "ip_address": "127.0.0.0",
-            "token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjEsImlzcyI6Imh0dHA6XC9cL2xvY2FsaG9zdFwvYXBpXC9yZWdpc3RlciIsImlhdCI6IjE0NDc5MzQ2OTkiLCJleHAiOiIxNDQ3OTM4Mjk5IiwibmJmIjoiMTQ0NzkzNDY5OSIsImp0aSI6IjI5ZTcwYjczNDI4MjM3ZTk0MWQzNjg2ODI3NGRhMzFiIn0.iT9VsNdo7w8kl2eyIewKTtGihT303wMOY-PdxYj81aI",
-            "created_at": "2015-11-19 12:04:59",
-            "updated_at": "2015-11-19 12:04:59"
-        });
+        var error = function (response) {
+            $scope.errors = response;
+        };
+
+        var options = {
+            'url': 'register',
+            'data': {
+                nickname: $scope.nickname
+            }
+        };
+        api.post(options).success(success).error(error);
     };
-
-
 }]);
